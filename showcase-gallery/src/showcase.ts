737fb10a92556704f588c62baa39c081b96d90cd
Ravi Dev Pandey <pandeyravidev@gmail.com>
2023-06-07 12:02:05 +0530

    create showcase
    Signed-off-by: Ravi Dev Pandey <pandeyravidev@gmail.com>

---
+++ showcase-gallery/src/showcase.ts
@@ -0,0 +1,10 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit-element";
+import { FormulaEditor } from "../../packages/formula-editor/src/formula-editor";
+
+@customElement("showcase-gallery")
+class ShowcaseGallery extends LitElement {
+  render() {
+    html` <formula-editor></formula-editor> `;
+  }
+}
